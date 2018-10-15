84d6fd02fccf40dc33896756edc19b3f0f1a91f6
Leif Lampater <leiflampater@gmail.com>
2018-10-15 18:43:17 +0200

    Enemy und Player als PNG Freigestellt

--- src/de/awacademy/invaders/model/SpaceshipEnemy.java
+++ src/de/awacademy/invaders/model/SpaceshipEnemy.java
@@ -5,7 +5,7 @@ import javafx.scene.image.Image;
 public class SpaceshipEnemy {
 
     private double posY,posX;
-    private Image image = new Image("de/awacademy/invaders/model/images/EnemySpaceship.jpg");
+    private Image image = new Image("de/awacademy/invaders/model/images/EnemySpaceship.png");
 
     public SpaceshipEnemy(double posY, double posX) {
         this.posY = posY;
--- src/de/awacademy/invaders/model/SpaceshipPlayer.java
+++ src/de/awacademy/invaders/model/SpaceshipPlayer.java
@@ -5,7 +5,7 @@ import javafx.scene.image.Image;
 public class SpaceshipPlayer {
 
     private double posX,posY;
-    private Image image=new Image("de/awacademy/invaders/model/images/SpaceShipPlayer.jpg");
+    private Image image=new Image("de/awacademy/invaders/model/images/SpaceShipPlayer.png");
 
     public SpaceshipPlayer(double posX, double posY) {
         this.posY = posY;
